86584b3e41bef9b1b02d346b16bcb8af6004a9ec
Solange Mukamurenzi <murenzisoso@yahoo.com>
2024-04-09 11:12:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const input = prompt(" Enter the maximum number:");
+console.log(input);
+
+const divisor_1 = prompt(" Enter the first divisor:");
+console.log(input);
+
+const divisor_2= prompt(" Enter the second divisor:");
+console.log(input);
+
+for (let i=1; i<=input; i++)
+{
+if ((i % divisor_1== 0) && (i % divisor_2 == 0)){
+   console.log("Bish-Bosh");
+}
+
+else if(i % divisor_1 == 0){
+   console.log("Bish");
+}
+
+else if (i % divisor_2 == 0){
+   console.log("Bosh");
+}
+
+else console.log(i);
+}
